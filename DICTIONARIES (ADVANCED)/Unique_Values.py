76a896bd0bd34574e3ebeeb2f006af3b1c3fe81f
ARBUCHELI <radio_seattle@hotmail.com>
2023-02-02 18:51:59 -0500

    adding Unique Values

--- DICTIONARIES (ADVANCED)/Unique_Values.py
+++ DICTIONARIES (ADVANCED)/Unique_Values.py
@@ -1,14 +1,12 @@
-# Write your frequency_dictionary function here:
-def frequency_dictionary(words):
-  result = {}
-  for word in words:
-    if result.get(word) == None:
-      result[word] = 1
-    elif result.get(word) != None:
-      result[word] = result[word] + 1
-  return result
+# Write your unique_values function here:
+def unique_values(my_dictionary):
+  result = []
+  for value in my_dictionary.values():
+    if value not in result:
+      result.append(value)
+  return len(result)
 # Uncomment these function calls to test your  function:
-print(frequency_dictionary(["apple", "apple", "cat", 1]))
-# should print {"apple":2, "cat":1, 1:1}
-print(frequency_dictionary([0,0,0,0,0]))
-# should print {0:5}
+print(unique_values({0:3, 1:1, 4:1, 5:3}))
+# should print 2
+print(unique_values({0:3, 1:3, 4:3, 5:3}))
+# should print 1
